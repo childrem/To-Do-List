2fc3dd05bf88d02d03ceb82a0afd5ffe92bd9e04
childrem <38900005+childrem@users.noreply.github.com>
2018-11-13 17:45:56 -0800

    Update toDo.js

--- toDo.js
+++ toDo.js
@@ -1,1 +1,32 @@
+var express = require('express');
 
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 6897);
+
+
+
+
+app.use(function(req,res){
+   res.status(404);
+   res.render('404');
+});
+
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
